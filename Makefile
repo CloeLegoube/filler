ad84149b1c42ccfbf1409e5a50bf89443fabbe4d
Cloe LEGOUBE <clegoube@e2r8p9.42.fr>
2017-04-03 19:43:21 +0200

    Initial commit with 1st files

---
+++ Makefile
@@ -0,0 +1,48 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jjaouen <jjaouen@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2016/12/07 16:55:10 by jjaouen           #+#    #+#              #
+#    Updated: 2017/04/03 18:04:17 by clegoube         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+GCCF = gcc -Wall -Wextra -Werror
+
+NAME = clegoube.filler
+
+HEAD = filler.h
+
+SRC = main.c get_next_line.c
+
+LIB = ./libft/libft.a
+
+OBJ = $(SRC:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	Make -C ./libft/
+	$(GCCF) $(SRC) $(LIB) -I $(HEAD) -o $(NAME)
+
+%.o: %.c
+	$(GCCF) -c $<
+
+clean:
+	Make clean -C ./libft/
+	rm -Rf $(OBJ)
+
+fclean: clean
+	rm -rf $(LIB)
+	rm -rf $(NAME)
+	rm -rf players/$(NAME)
+
+re: fclean all
+
+run: re
+	cp $(NAME) players/$(NAME)
+
+.PHONY: all clean
---
+++ filler.h
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filler.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjaouen <jjaouen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/30 13:00:28 by jjaouen           #+#    #+#             */
+/*   Updated: 2017/04/03 19:37:13 by clegoube         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLER_H
+# define FILLER_H
+# define BUFF_SIZE 235
+
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+
+typedef struct		s_coor
+{
+	char			letter;
+	int				*x;
+	int				*y;
+	struct s_coor	*next;
+}					t_coor;
+
+typedef struct		s_var
+{
+	int				fd;
+	int				ret;
+	size_t			loop;
+	size_t			x_first;
+	size_t			y_first;
+	char			letter;
+	size_t			line;
+	size_t			col;
+	size_t			i;
+	t_list			*new;
+	int				*y;
+	int				*x;
+	int				n;
+}					t_var;
+
+typedef struct		s_game
+{
+	int		player;
+	int		X_lenght;
+	int		piece_lenght;
+	int		Y_width;
+	int		piece_width;
+	char	symbol;
+}					t_game;
+
+int		get_next_line(const int fd, char **line);
+char	**ft_strsplit(char const *s, char c);
+
+#endif
